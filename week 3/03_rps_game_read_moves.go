ba4bd53d4b33adff0267d81111eb311a781b83ea
s06a <91134347+s06a@users.noreply.github.com>
2024-06-03 16:49:40 +0330

    [Add] simple code to read rps move and show the result

---
+++ week 3/03_rps_game_read_moves.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var move rune // rune stores one character
+	fmt.Println("Wellcome to Rock Paper Scissors game!")
+	fmt.Println("Choose your move: R, P, or S")
+	fmt.Scanf("%c", &move)
+	if move == 'R' { // important to use ' instead of "
+		fmt.Println("your move is R")
+	} else if move == 'P' {
+		fmt.Println("your move is P")
+	} else if move == 'S' {
+		fmt.Println("your move is S")
+	} else {
+		fmt.Println("illegal move!")
+	}
+}
