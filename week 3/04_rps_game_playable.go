4b595ef1c5018f3545386bc384e74d6639495219
s06a <91134347+s06a@users.noreply.github.com>
2024-06-03 17:55:44 +0330

    [Add] rps with if else conditions

---
+++ week 3/04_rps_game_playable.go
@@ -0,0 +1,43 @@
+/* rock paper scissors code to just read
+   user's move
+*/
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	var move rune // rune stores one character
+
+	fmt.Println("Wellcome to Rock Paper Scissors game!")
+	fmt.Println("Choose your move: R, P, or S")
+	fmt.Scanf("%c", &move)
+
+	if move == 'R' { // important to use ' instead of "
+		fmt.Println("your move is R")
+	} else if move == 'P' {
+		fmt.Println("your move is P")
+	} else if move == 'S' {
+		fmt.Println("your move is S")
+	} else {
+		fmt.Println("illegal move!")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	machineMove := rand.Intn(3) // 0 for rocke, 1 for paper, 2 for scissors
+
+	if machineMove == 0 && move == 'S' {
+		fmt.Println("machine wins")
+	} else if machineMove == 1 && move == 'R' {
+		fmt.Println("machine wins")
+	} else if machineMove == 2 && move == 'P' {
+		fmt.Println("machine wins")
+	} else {
+		fmt.Println("draw or you win!")
+	}
+}
