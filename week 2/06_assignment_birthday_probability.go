d3423f0ccb6333c2e2dab152c4f7231fdceed6b0
s06a <91134347+s06a@users.noreply.github.com>
2024-06-03 16:19:56 +0330

    [Add] 06_assignemnt_birthday_probability.go

---
+++ week 2/06_assignment_birthday_probability.go
@@ -0,0 +1,27 @@
+/* Assume a room with 20 people. What is the probability that two of
+   them have the same birthday? For simplicity, say a year has 365 days.
+   Day 1 is January 1 and day 365 is December 31.
+
+   So, a birthday will be a number from 1 to 365.  Now compute whether 20
+   people at random will have the same birthday. Do this 10000 times to
+   get a reasonable sample.  Also do this for 10, 20, 30 ,.. 100 people
+   in the room and print this as a table.
+
+	 limitations: cause arrays and functions are not taught till this
+	 assignment, I assume that we have to use basic tools such as loops,
+	 variables, and if condition!
+*/
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	/* not sure how to do it using monte carlo and not using slices,
+	   functions, and ...
+	*/
+}
