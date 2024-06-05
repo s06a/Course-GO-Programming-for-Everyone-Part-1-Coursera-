bbbc5dbb749a8deaec35a190cbf5a6d9b22387a1
s06a <91134347+s06a@users.noreply.github.com>
2024-06-05 23:58:06 +0330

    [Add] not started yet

---
+++ week 4/06_assignment_salaries.go
@@ -0,0 +1,22 @@
+/* Write a GO program that examines employee’s salaries stored in a map. The map
+   has employee name as the key and salary as an int value. You wish to find and
+   print the employee name with the smallest salary, and the employee name with
+   the largest salary as well as the company’s average salary.
+
+   This program should generate the map by reading, from the file provided below,
+   the name and salary of employees.
+
+   Its form will be first_name last_ name salary (string string int) e.g. Ira Pohl
+   77700
+
+   It will be one per line in the file.
+
+   If you use a map, then you can make a string the key and the salary the value
+   and use the names concatenated as the key, e.g. Ira.Pohl 77700 will be the map
+   element.
+
+   Alternatively, you could read the name and salary into two arrays. You could
+   process the two arrays to find out these answers.
+*/
+
+// not started yet
