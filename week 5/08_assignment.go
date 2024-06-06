152dbe7aa08448779c3d4f9fcd7bf80f390e43cd
s06a <91134347+s06a@users.noreply.github.com>
2024-06-06 21:55:10 +0330

    [Add] question

---
+++ week 5/08_assignment.go
@@ -0,0 +1,7 @@
+/* Write a GO program that, as explained in our video, does mergesort. There are
+   several ways to do this, but best marks will be given if you use one slice
+   that starts with the unordered data. The program should generate appropriate
+   int test data pseudo-randomly. Do this for 10,000 elements.
+*/
+
+// not started yet
